2a60a26a536ba433f93e953e29c5a08aeaccfc19
gojnimer6553 <gojnimer@gmail.com>
2023-10-09 23:27:46 -0300

    minor

--- src/contexts/global/index.tsx
+++ src/contexts/global/index.tsx
@@ -1,12 +1,11 @@
 import { FunctionComponent, createContext } from "preact";
-import Loader from "../../components/loader";
-import { useReducer } from "preact/hooks";
+/* import { useReducer } from "preact/hooks"; */
 
 const GlobalContext = createContext({});
 
 const GlobalProvider: FunctionComponent = ({ children }) => {
   //Adicionar loading futuramente
-  const [active, loader] = useReducer((a: boolean, _b: string) => !a, true);
+  /*  const [active, loader] = useReducer((a: boolean, _b: string) => !a, true); */
   return (
     <GlobalContext.Provider value={{}}>
       {children}
